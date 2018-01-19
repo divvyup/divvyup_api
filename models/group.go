395d26da258285d175cee77849db9ee4380e4f2a
dom <dominic.dipasquale@gmail.com>
2018-01-19 13:20:36 +1000

    Add response JSON models for both users and groups

--- models/group.go
+++ models/group.go
@@ -8,3 +8,13 @@ type Group struct {
 	Name string `json:"name"`
 	ID   int64  `json:"id"`
 }
+
+/*
+	GroupJSON is the model we use to respond to a request about a
+	group, it gives useful information for the fron end
+*/
+type GroupJSON struct {
+	Name    string     `json:"name"`
+	ID      int64      `json:"id"`
+	Members []UserJSON `json:"members"`
+}
--- models/user.go
+++ models/user.go
@@ -9,3 +9,13 @@ type User struct {
 	Password string `json:"password"`
 	ID       int64  `json:"id"`
 }
+
+/*
+	UserJSON is the model we will use for sending user information
+	to the clients. We do not want to be sending the password and
+	id, but often will want to spend their balance
+*/
+type UserJSON struct {
+	Username string  `json:"name"`
+	Balance  float64 `json:"balance"`
+}
